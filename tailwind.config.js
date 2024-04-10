2fe6433911784b31d49b4899cfcb340c6429a8d4
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-04-10 18:46:17 +0000

    style: Add Julius Sans One font to App.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        julius: ["Julius Sans One"],
+      },
+    },
   },
   plugins: [],
 };
